bc050c34a1885b9637d8f18ae405f697e3b2300c
jcl <chaelynjang@lotte.net>
2024-02-20 14:58:11 +0900

    스프링 배치 튜토리얼 프로젝트 생성

---
+++ src/main/java/com/example/springbatchtutorial/SpringBatchTutorialApplication.java
@@ -0,0 +1,15 @@
+package com.example.springbatchtutorial;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing // 배치 구동
+@SpringBootApplication
+public class SpringBatchTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchTutorialApplication.class, args);
+	}
+
+}
